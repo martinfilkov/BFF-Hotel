570437db571ce819d0e0f636ccb4dafa1358ac8e
martinfilkov <martinfilkov1@gmail.com>
2024-08-06 15:36:09 +0300

    initial commit

--- domain/src/main/java/com/tinqinacademy/bff/domain/configurations/HotelRestExportConfiguration.java
+++ domain/src/main/java/com/tinqinacademy/bff/domain/configurations/HotelRestExportConfiguration.java
@@ -3,6 +3,6 @@ package com.tinqinacademy.bff.domain.configurations;
 import com.tinqinacademy.hotel.restexport.HotelRestClient;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "hotel-service", url = "${hotel.service.url}")
+@FeignClient(value = "hotel-service", url = "${hotel.service.url}", configuration = ClientConfiguration.class)
 public interface HotelRestExportConfiguration extends HotelRestClient {
 }
